36fe849d88701c783ca389a41e01fba74fc4fbbb
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-08-22 10:48:23 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/d/Test2Application.java
@@ -0,0 +1,12 @@
+package d;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Test2Application.class, args);
+	}
+
+}
